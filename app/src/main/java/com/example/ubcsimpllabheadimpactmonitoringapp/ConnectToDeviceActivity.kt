a381e2005f0bc72b369d57a7da2c9b2d16151206
MickAvery <mav.cuyugan@gmail.com>
2020-09-23 22:29:55 -0700

    added home screen

---
+++ app/src/main/java/com/example/ubcsimpllabheadimpactmonitoringapp/ConnectToDeviceActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ubcsimpllabheadimpactmonitoringapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConnectToDeviceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_connect_to_device)
+    }
+}
