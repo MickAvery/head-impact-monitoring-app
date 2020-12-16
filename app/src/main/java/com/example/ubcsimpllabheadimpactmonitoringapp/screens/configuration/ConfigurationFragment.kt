1eef1183663d65718bc5667f071122fd09b5a09f
MickAvery <mav.cuyugan@gmail.com>
2020-12-15 19:21:53 -0800

    better configuration screen layout arrangement, also some values in datalog mode dropdown

--- app/src/main/java/com/example/ubcsimpllabheadimpactmonitoringapp/screens/configuration/ConfigurationFragment.kt
+++ app/src/main/java/com/example/ubcsimpllabheadimpactmonitoringapp/screens/configuration/ConfigurationFragment.kt
@@ -6,7 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.example.ubcsimpllabheadimpactmonitoringapp.R
+import com.example.ubcsimpllabheadimpactmonitoringapp.databinding.ConfigurationFragmentBinding
 
 class ConfigurationFragment : Fragment() {
 
@@ -15,12 +18,30 @@ class ConfigurationFragment : Fragment() {
     }
 
     private lateinit var viewModel: ConfigurationViewModel
+    private lateinit var mBinding: ConfigurationFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.configuration_fragment, container, false)
+        /* get ViewBinding */
+        mBinding = ConfigurationFragmentBinding.inflate(layoutInflater)
+
+        /* set ActionBar title */
+        activity?.title = "Configure Device"
+
+        /* set Spinner contents */
+        val datalogModeSpinner: Spinner = mBinding.spinnerDatalogMode
+        ArrayAdapter.createFromResource(
+            requireActivity().applicationContext,
+            R.array.string_array_datalog_mode_options,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            datalogModeSpinner.adapter = adapter
+        }
+
+        return mBinding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
